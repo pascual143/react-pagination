57a18f261379dd4a84b7e5d9952055e493664bbf
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-05-15 20:01:50 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import PassengersList from './Passengers';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+       <PassengersList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
